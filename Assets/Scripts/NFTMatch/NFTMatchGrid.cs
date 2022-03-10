8f1d30f9c8c2459cfd18a439480d7695ea892801
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2022-03-10 11:18:02 +0000

    Got the basic grid working

--- Assets/Scripts/NFTMatch/NFTMatchGrid.cs
+++ Assets/Scripts/NFTMatch/NFTMatchGrid.cs
@@ -3,13 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NFTMatchGrid : MonoBehaviour {
+	[Header("Objects and references")]
     [SerializeField] private GameObject NFTPrefab;
+	[SerializeField] private TextAsset gridDataAsset;
+
+	[Header("Misc")]
+	[SerializeField] private int size;
+
+	private GameObject[] NFTGrid;
+
+	private enum Types {
+		Red,
+		Green,
+		Blue,
+		Yellow
+	}
+	private Types[] NFTGridData;
+	private Types[] startData;
 
 	private void Awake() {
-		
+		string data = gridDataAsset.text;
+		startData = new Types[size * size];
+		for (int i = 0; i < data.Length; i++) {
+			startData[i] = (Types)int.Parse(data[i].ToString());
+		}
+
+		GenerateGrid();
+		RenderGrid();
 	}
 
 	private void GenerateGrid() {
+		NFTGridData = (Types[])startData.Clone();
+		// TODO: rotate and flip randomly
+	}
+	private void RenderGrid() {
+		NFTGrid = new GameObject[size * size];
 
+		int end = size / 2;
+		int start = -end;
+
+		int i = 0;
+		for (int y = end; y > start; y--) {
+			for (int x = start; x < end; x++) {
+				GameObject NFT = Instantiate(NFTPrefab);
+				NFT.transform.parent = transform;
+				NFT.transform.position = new Vector2(x + 0.5f, y - 0.5f);
+				NFTMatchNFT NFTScript = NFT.GetComponent<NFTMatchNFT>();
+				NFTScript.type = (NFTMatchNFT.Types)NFTGridData[i];
+				NFTScript.Ready();
+
+				NFTGrid[i] = NFT;
+
+				i++;
+			}
+		}
 	}
 }
--- Assets/Scripts/NFTMatch/NFTMatchNFT.cs
+++ Assets/Scripts/NFTMatch/NFTMatchNFT.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NFTMatchNFT : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class NFTMatchNFT : MonoBehaviour {
+    [HideInInspector] public enum Types {
+		Red,
+		Green,
+		Blue,
+		Yellow
+	}
+	[HideInInspector] public Types type;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private SpriteRenderer ren;
+
+	private void Awake() {
+		ren = GetComponent<SpriteRenderer>();
+	}
+
+	public void Ready() {
+		Color[] colors = {
+			Color.red,
+			Color.green,
+			Color.blue,
+			Color.yellow
+		};
+
+		ren.color = colors[(int)type];
+	}
 }
