15c0fd9835dcd2da27c57ee2c899744bb6e0dabe
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2022-05-16 23:08:54 +0100

    Fixed some minor issues
    I still need to fix the glitchy text thing

--- Assets/Scripts/DNG/ChestController.cs
+++ Assets/Scripts/DNG/ChestController.cs
@@ -38,7 +38,7 @@ public class ChestController : MonoBehaviour {
 	private int spotlightTick;
 	private NFTController NFTOb;
 	private bool animating;
-	private int dailyLimitProgress = 4;
+	private int dailyLimitProgress;
 
 	private enum States {
 		WaitToOpen,
